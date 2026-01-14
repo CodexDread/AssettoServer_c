eeb3fb81d1b02185d6c4b64f47ea5ebfcbeca1e9
CodexDread <piatt.albert12@gmail.com>
2026-01-13 21:34:12 -0600

    SXR plugin development started

---
+++ TimeDilationPlugin/TimeDilationConfigurationValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using JetBrains.Annotations;
+
+namespace TimeDilationPlugin;
+
+[UsedImplicitly]
+public class TimeDilationConfigurationValidator : AbstractValidator<TimeDilationConfiguration>
+{
+    public TimeDilationConfigurationValidator()
+    {
+        RuleFor(cfg => cfg.SunAngleLookupTable).NotEmpty().Unless(cfg => cfg.TimeLookupTable.Count > 0);
+        RuleForEach(cfg => cfg.SunAngleLookupTable).ChildRules(salut =>
+        {
+            salut.RuleFor(s => s.TimeMult).GreaterThanOrEqualTo(0);
+        });
+        
+        RuleFor(cfg => cfg.TimeLookupTable).NotEmpty().Unless(cfg => cfg.SunAngleLookupTable.Count > 0);
+        RuleForEach(cfg => cfg.TimeLookupTable).ChildRules(tlut =>
+        {
+            tlut.RuleFor(t => t.Time).Matches(@"^(?:1?\d|2[0-3]):(?:[0-5]\d)$");
+            tlut.RuleFor(t => t.TimeMult).GreaterThanOrEqualTo(0);
+        });
+    }
+}
